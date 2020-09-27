608a2ec0895f5af46a13a12e35f971b83042adc2
samumar43 <samumar43@gmail.com>
2020-09-27 17:39:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,26 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+return sum
 end
 
 def subtraction(num1, num2)
-
+difference = num1 - num2
+return difference
 end
 
 def division(num1, num2)
-
+quotient = num1 / num2
+return quotient
 end
 
 def multiplication(num1, num2)
-
+product = num1 * num2
+return product
 end
 
 def modulo(num1, num2)
-
+reverse_quotient = num2 / num1
+remainder = reverse_quotient
 end
 
 def square_root(num)
